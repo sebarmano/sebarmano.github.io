bc2e4f48f4f01463bbd4b1b6afb46a32a34ce72f
Sebastian Armano <sebarmano@gmail.com>
2016-02-10 11:31:16 -0500

    Add menu

--- Gemfile
+++ Gemfile
@@ -2,10 +2,6 @@
 # the following line to use 'http://'
 source 'https://rubygems.org'
 
-source "https://rails-assets.org" do
-  gem "rails-assets-jquery"
-end
-
 # For faster file watcher updates on Windows:
 gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,4 @@
-//= require jquery
-//= require_tree .
-
 $(document).ready(function() {
-  console.log("Here");
   var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
   $('#js-centered-navigation-menu').removeClass("show");
 
@@ -13,5 +9,20 @@ $(document).ready(function() {
         $('#js-centered-navigation-menu').removeAttr('style');
       }
     });
+  });
+});
+
+$(document).ready(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+    }
   });
 });
